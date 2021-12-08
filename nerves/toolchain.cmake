8d747c97374f237670d6d8322b71e6565ca36a1c
Cocoa <89497197+cocoa-xu@users.noreply.github.com>
2021-12-08 10:19:53 +0000

    Update toolchain.cmake

--- nerves/toolchain.cmake
+++ nerves/toolchain.cmake
@@ -14,8 +14,8 @@ if(NOT "$ENV{TARGET_CPU}" STREQUAL "" AND NOT "$ENV{TARGET_ARCH}" STREQUAL "" AN
         set(CMAKE_FIND_ROOT_PATH "${NERVES_CC_PATH}/../${TOOLCHAIN_PREFIX}/sysroot")
 
         # disable nepn opt in libpng as the compiler was not compiled with neon enabled
-        set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -DPNG_ARM_NEON_OPT=0")
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DPNG_ARM_NEON_OPT=0")
+        set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fPIC")
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fPIC")
 
         # adjust the default behavior of the find commands:
         # search headers and libraries in the target environment
