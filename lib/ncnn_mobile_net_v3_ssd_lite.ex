98a8d0ced93cf942f2e66a55343ad117d12f2d89
Cocoa <89497197+cocoa-xu@users.noreply.github.com>
2021-12-08 10:48:00 +0000

    Update ncnn_mobile_net_v3_ssd_lite.ex

--- lib/ncnn_mobile_net_v3_ssd_lite.ex
+++ lib/ncnn_mobile_net_v3_ssd_lite.ex
@@ -5,9 +5,11 @@ defmodule Ncnn.MobileNetV3SsdLite do
     Ncnn.Net.load_model(net, model)
   end
 
-  def labels, do: ["background", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow",
-                "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train",
-                "tvmonitor"]
+  def labels, do: [
+                "background", "aeroplane", "bicycle", "bird", "boat",
+                "bottle", "bus", "car", "cat", "chair", "cow",
+                "diningtable", "dog", "horse", "motorbike", "person",
+                "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
 
   def predict(self, image_path) do
     Ncnn.Net.forward(self, image_path)
@@ -21,7 +23,7 @@ defmodule Ncnn.MobileNetV3SsdLite do
     if prob > threshold do
 #      {:ok, mat} = OpenCV.rectangle(mat, [x, y, w, h], [255, 0, 0])
       IO.puts Enum.at(labels(), label_index) <> ": #{Float.round(prob, 2)}"
-      # todo: OpenCV.gettextsize seems always return an invaild value
+      # todo: OpenCV.gettextsize seems always return an invalid value
 #      {:ok, {{label_height, label_weight}, baseline}} = OpenCV.gettextsize(label_text, OpenCV.cv_font_hershey_simplex, 0.5, 1)
 #      y = y - label_height - baseline
 #      if y < 0, do: y = 0
@@ -33,10 +35,10 @@ defmodule Ncnn.MobileNetV3SsdLite do
       translate_pred(rest, mat, cols, threshold)
     end
   end
-  def translate_pred([], mat, _), do: mat
+  def translate_pred([], mat, _, _), do: mat
 
   def translate_pred(source_mat, pred, threshold \\ 0.5) do
     {:ok, {_, cols, _}} = OpenCV.Mat.shape(source_mat)
-    visualise_pred(pred, source_mat, cols, threshold)
+    translate_pred(pred, source_mat, cols, threshold)
   end
 end
