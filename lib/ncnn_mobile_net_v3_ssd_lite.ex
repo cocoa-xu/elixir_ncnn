c4bb27c1810c6c69077d9724ba412dac4d3fe0f3
Cocoa <89497197+cocoa-xu@users.noreply.github.com>
2021-12-09 20:44:56 +0000

    image out

--- lib/ncnn_mobile_net_v3_ssd_lite.ex
+++ lib/ncnn_mobile_net_v3_ssd_lite.ex
@@ -21,21 +21,30 @@ defmodule Ncnn.MobileNetV3SsdLite do
 
   def translate_pred([{label_index, prob, {{x, y}, {w, h}}}|rest], mat, cols, threshold) do
     if prob > threshold do
-#      {:ok, mat} = OpenCV.rectangle(mat, [x, y, w, h], [255, 0, 0])
-      IO.puts Enum.at(labels(), label_index) <> ": #{Float.round(prob, 2)}"
-      # todo: OpenCV.gettextsize seems always return an invalid value
-#      {:ok, {{label_height, label_weight}, baseline}} = OpenCV.gettextsize(label_text, OpenCV.cv_font_hershey_simplex, 0.5, 1)
-#      y = y - label_height - baseline
-#      if y < 0, do: y = 0
-#      if x + label_weight > cols, do: x = cols - label_weight
-#      {:ok, mat} = OpenCV.rectangle(mat, [x, y], [x + label_weight, y + label_height + baseline], [255, 255, 255])
-#      {:ok, mat} = OpenCV.puttext(mat, label_text, [0, 0], OpenCV.cv_font_hershey_simplex, 0.5, [0, 0, 0])
+      x = trunc(x)
+      y = trunc(y)
+      w = trunc(w)
+      h = trunc(h)
+      {:ok, mat} = OpenCV.rectangle(mat, [x, y], [x + w, y + h], [255, 0, 0])
+      label_text = " " <> Enum.at(labels(), label_index) <> ": #{Float.round(prob, 2)} "
+      IO.puts label_text
+      {:ok, {{label_weight, label_height}, baseline}} = OpenCV.gettextsize(label_text, OpenCV.cv_font_hershey_simplex, 0.5, 1)
+      y = y - label_height - baseline
+      if y < 0, do: y = 0
+      if x + label_weight > cols, do: x = cols - label_weight
+      x = trunc(x)
+      y = trunc(y)
+      label_weight = trunc(label_weight)
+      label_height = trunc(label_height)
+      {:ok, mat} = OpenCV.rectangle(mat, [x, y], [x + label_weight, y + label_height + baseline], [255, 255, 255])
+      {:ok, mat} = OpenCV.puttext(mat, label_text, [x, y + label_height],
+        OpenCV.cv_font_hershey_simplex, 0.5, [0, 0, 255], thickness: 1)
       translate_pred(rest, mat, cols, threshold)
     else
       translate_pred(rest, mat, cols, threshold)
     end
   end
-  def translate_pred([], mat, _, _), do: mat
+  def translate_pred([], mat, _, _), do: {:ok, mat}
 
   def translate_pred(source_mat, pred, threshold \\ 0.5) do
     {:ok, {_, cols, _}} = OpenCV.Mat.shape(source_mat)
